e3ba8cd72fb1effb1b9b1c42a5aca153ff176209
Thalita Teixeira <thalitavst@gmail.com>
2020-06-22 16:12:23 -0300

    Primeiro commite do código

---
+++ estrutura-para-faca.js
@@ -0,0 +1,9 @@
+function acaoBotao() {
+    var numero, fatorial, contador
+numero = prompt ("Digite o número para calcular o fatorial: ")
+fatorial = 1
+for (contador = 1; contador <= numero; contador++) {
+    fatorial = fatorial * contador
+document.getElementById("paragrafo").innerText = "O fatorial de " + numero + " é: " + fatorial   
+   }
+}
